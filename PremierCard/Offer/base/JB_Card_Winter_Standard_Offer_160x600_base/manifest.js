c56180ce8f854734e9d134deacab28bb97f3cb5f
Mahesh Bhatt <141926871+omnicomproductionstudios@users.noreply.github.com>
2025-09-15 22:56:07 +0530

    Add files via upload

---
+++ PremierCard/Offer/base/JB_Card_Winter_Standard_Offer_160x600_base/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 160,
+  "height": 600,
+  "clickTagCount": 1,
+  "hideBrowsers": ["ie8"],
+  "richloads": [
+   {"name":"main_rl", "src":"JB_Card_Winter_Standard_Offer_160x600_RL"}
+ ],
+  "instantAds": [
+      {"name":"main_rl",          "type":"richload"},
+      {"name":"clickTag1_url",    "type":"text", "default":"https://www.jetblue.com/trueblue/credit-cards/jetblue-card-comparison"}
+  ]
+});
