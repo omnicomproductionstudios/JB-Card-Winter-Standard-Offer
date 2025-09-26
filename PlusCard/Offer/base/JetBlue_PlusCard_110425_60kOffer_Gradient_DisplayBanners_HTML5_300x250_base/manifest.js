253a13e12cfc1f196320cf163402168bdfa7c213
Mahesh Bhatt <141926871+omnicomproductionstudios@users.noreply.github.com>
2025-09-26 21:23:54 +0530

    Add files via upload

---
+++ PlusCard/Offer/base/JetBlue_PlusCard_110425_60kOffer_Gradient_DisplayBanners_HTML5_300x250_base/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 300,
+  "height": 250,
+  "clickTagCount": 1,
+  "hideBrowsers": ["ie8"],
+  "richloads": [
+   {"name":"main_rl", "src":"JetBlue_PlusCard_110425_60kOffer_Gradient_DisplayBanners_HTML5_300x250_RL"}
+ ],
+  "instantAds": [
+      {"name":"main_rl",          "type":"richload"},
+      {"name":"clickTag1_url",    "type":"text", "default":"https://www.jetblue.com/trueblue/credit-cards/jetblue-card-comparison"}
+  ]
+});
